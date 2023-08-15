8d76e991022130c9c2b1cbb72a7a5163877484bc
Thomas Lam <137445487+ThomasLam2142@users.noreply.github.com>
2023-08-14 22:40:26 -0400

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,12 @@
+if(document.readyState == 'loading'){
+    document.addEventListener('DOMContentLoaded',ready)
+} else{
+    ready()
+}
+
+function ready (){
+    
+}
+
+
+//document.getElementsByClassName('btn')[0].addEventListener('click',purchaseClicked)
