7a8813cf49729e075267af78c1e8404ddd609c40
ThomasLam2142 <thomaslam2142@gmail.com>
2023-08-16 16:26:56 -0400

    basic todo Functionality completed

--- todo.js
+++ todo.js
@@ -1,25 +1,29 @@
-let obj = {};
+let obj = [];
 
 function myFunction(){
     console.log("button pressed")
     let text = document.getElementsByClassName('text-box')[0].value;
     console.log(text)
-    obj = obj + JSON.stringify(text)
+    obj.push(text)
     console.log(obj)
     addElement(obj)
 }
 
 function addElement(obj) {
     // create a new div element
+
+    let objlen = obj.length;
+    console.log(objlen)
     const newDiv = document.createElement("li");
   
     // and give it some content
-    const newContent = document.createTextNode(obj);
+    const newContent = document.createTextNode(obj[objlen-1]);
   
     // add the text node to the newly created div
     newDiv.appendChild(newContent);
   
     // add the newly created element and its content into the DOM
-    const currentDiv = document.getElementById("div1");
+    const currentDiv = document.getElementById("todo-pop");
     document.body.insertBefore(newDiv, currentDiv);
+    
   }
