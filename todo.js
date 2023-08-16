4cdc704c1f9c93be139960d9a4c57b0685ff24cc
ThomasLam2142 <thomaslam2142@gmail.com>
2023-08-16 10:45:16 -0400

    basic JSON data structures implemented

--- todo.js
+++ todo.js
@@ -1,6 +1,10 @@
+let obj = {};
+
 function myFunction(){
-    console.log("button works")
+    console.log("button pressed")
     let text = document.getElementsByClassName('text-box')[0].value;
     console.log(text)
+    obj = obj + JSON.stringify(text)
+    console.log(obj)
 }
 
