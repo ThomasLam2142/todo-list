900ce95159772f27f5dc2d9b564a726fcfb1f6a7
ThomasLam2142 <thomaslam2142@gmail.com>
2023-08-16 16:04:15 -0400

    Got the List printing using json

--- todo.js
+++ todo.js
@@ -6,5 +6,20 @@ function myFunction(){
     console.log(text)
     obj = obj + JSON.stringify(text)
     console.log(obj)
+    addElement(obj)
 }
 
+function addElement(obj) {
+    // create a new div element
+    const newDiv = document.createElement("li");
+  
+    // and give it some content
+    const newContent = document.createTextNode(obj);
+  
+    // add the text node to the newly created div
+    newDiv.appendChild(newContent);
+  
+    // add the newly created element and its content into the DOM
+    const currentDiv = document.getElementById("div1");
+    document.body.insertBefore(newDiv, currentDiv);
+  }
